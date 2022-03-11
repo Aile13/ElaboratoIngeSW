c0584947beb01a0ff06fd6674030a1803e97989d
ljAli <47888612+ljAli@users.noreply.github.com>
2022-03-11 16:35:21 +0100

    Update Categoria.java

--- src/java/it/unibs/elabingesw/businesslogic/categoria/Categoria.java
+++ src/java/it/unibs/elabingesw/businesslogic/categoria/Categoria.java
@@ -3,13 +3,14 @@ package it.unibs.elabingesw.businesslogic.categoria;
 import java.util.List;
 
 /**
+ * Classe Categoria che rappresenta una categoria generica
  * @author Elia Pitozzi
  */
 public class Categoria {
     private final String nome;
     private final String descrizione;
     private final List<Campo> campiNativi;
-
+    
     public Categoria(String nome, String descrizione, List<Campo> campiNativi) {
         this.nome = nome;
         this.descrizione = descrizione;
