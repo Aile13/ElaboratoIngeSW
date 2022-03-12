d97effe39a1e0943e83989ebf3965c739b430d5a
ljAli <47888612+ljAli@users.noreply.github.com>
2022-03-12 12:40:05 +0100

    Javadoc Login.java

--- src/java/it/unibs/elabingesw/service/Login.java
+++ src/java/it/unibs/elabingesw/service/Login.java
@@ -4,16 +4,36 @@ import it.unibs.elabingesw.businesslogic.gestione.GestoreUtenti;
 import it.unibs.eliapitozzi.mylib.InputDati;
 
 /**
- * @author Elia
+ * Classe Login per la gestione degli accessi all'applicativo
+ * da parte di configuratori e utenti.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class Login {
     private final GestoreUtenti gestoreUtenti;
-
+    
+    /**
+     * Costruttore di classe, accetta come parametro un oggetto
+     * GestoreUtenti.
+     * 
+     * @param gestoreUtenti
+     * @see GestoreUtenti
+     */ 
     public Login(GestoreUtenti gestoreUtenti) {
         this.gestoreUtenti = gestoreUtenti;
     }
 
-
+    /**
+     * Metodo per l'effettuazione del login: vengono chieste le
+     * credenziali e si effettuano vari controlli.
+     * <p>
+     * Si controlla se l'utente inserito è già presente all'in-
+     * terno dell'applicativo e, successivamente, se il configu-
+     * ratore è quello di default o meno: se lo è, si viene ri-
+     * mandati alla procedura di creazione di un nuovo configu-
+     * ratore.
+     */
     public void eseguiLogin() {
         boolean ricontrolla;
         do {
@@ -42,7 +62,12 @@ public class Login {
         } while (ricontrolla);
 
     }
-
+    
+    /**
+     * Metodo che permette di creare un nuovo configuratore: devo-
+     * no essere inseriti uno username adatto (non già utilizzato
+     * da un configuratore già registrato) e una password.
+     */
     private void creaNuovoConfiguratore() {
 
         System.out.println("Procedura di creazione nuovo configuratore avviata.");
