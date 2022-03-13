e40a4baab56730148c2c8825dd6d2ef76023b82f
ljAli <47888612+ljAli@users.noreply.github.com>
2022-03-13 13:20:19 +0100

    Javadoc Utente,java
    Controllare il javadoc del metodo hashCode().

--- src/java/it/unibs/elabingesw/businesslogic/utente/Utente.java
+++ src/java/it/unibs/elabingesw/businesslogic/utente/Utente.java
@@ -5,30 +5,68 @@ import it.unibs.elabingesw.businesslogic.gestione.Manageable;
 import java.util.Objects;
 
 /**
- * @author Elia
+ * Classe Utente che rappresenta un generico utente. Questa classe
+ * verrà estesa dalla classe Configuratore.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public abstract class Utente implements Manageable {
     private final String username;
     private final String password;
-
+    
+    /**
+     * Costruttore di classe che accetta come parametri uno username
+     * e una password.
+     *
+     * @param username
+     * @param password
+     */
     public Utente(String username, String password) {
         this.username = username;
         this.password = password;
     }
-
+    
+    /**
+     * Metodo implementato dall'interfaccia Manageable
+     * che verifica se due username sono uguali.
+     *
+     * @param nome lo username da paragonare
+     * @return TRUE se gli username sono uguali
+     *         FALSE se gli username sono diversi
+     */
     @Override
     public boolean isStessoNome(String nome) {
         return this.username.equals(nome);
     }
-
+    
+    /**
+     * Metodo che controlla se la password inserita dall'utente
+     * è corretta.
+     *
+     * @param pwd la password dell'utente
+     * @return TRUE se la password è corretta
+     *         FALSE se la password è errata
+     */
     public boolean isPasswordCorretta(String pwd) {
         return this.password.equals(pwd);
     }
-
+    
+    /**
+     * Metodo getter.
+     *
+     * @return lo username dell'utente
+     */
     public String getUsername() {
         return username;
     }
-
+    
+    /**
+     * Metodo per la formattazione che converte un oggetto nella re-
+     * lativa rappresentazione di stringa.
+     *
+     * @return stringa dell'oggetto convertito
+     */
     @Override
     public String toString() {
         return "Utente{" +
@@ -36,7 +74,14 @@ public abstract class Utente implements Manageable {
                 ", password='" + password + '\'' +
                 '}';
     }
-
+    
+    /**
+     * Metodo che permette di confrontare due oggetti.
+     *
+     * @param o un oggetto generico
+     * @return TRUE se i due oggetti sono uguali
+     *         FALSE se i due oggetti sono diversi
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -44,7 +89,12 @@ public abstract class Utente implements Manageable {
         Utente utente = (Utente) o;
         return getUsername().equals(utente.getUsername());
     }
-
+    
+    /**
+     * Metodo che fornisce il codice hash dell'oggetto.
+     *
+     * @return l'hashcode dell'oggetto
+     */
     @Override
     public int hashCode() {
         return Objects.hash(getUsername());
