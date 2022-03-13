a568442f152b4dcb0a7e5b2864b191273d62cff6
ljAli <47888612+ljAli@users.noreply.github.com>
2022-03-13 12:24:03 +0100

    Javadoc Tree.java

--- src/java/it/unibs/elabingesw/businesslogic/categoria/Tree.java
+++ src/java/it/unibs/elabingesw/businesslogic/categoria/Tree.java
@@ -4,27 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author Elia
+ * Classe Tree che serve per rappresentare una generica struttura
+ * ad albero.
+ * 
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public final class Tree<T> {
     private T dato;
     //private Tree<T> padre;
     private final List<Tree<T>> listaFigli;
-
+    
+    /**
+     * Costruttore di classe che accetta come parametro un generico
+     * dato (nel nostro caso sarà una categoria).
+     * 
+     * @param dato 
+     */
     public Tree(T dato) {
         this.dato = dato;
         this.listaFigli = new ArrayList<>();
     }
-
+    
+    /**
+     * Metodo getter.
+     * 
+     * @return il dato
+     */
     public T getDato() {
         return dato;
     }
-
-    //    public boolean isRoot() {
-//        return this.padre == null;
-//    }
-
-
+    
+    /*
+    public boolean isRoot() {
+        return this.padre == null;
+    }
+    */
+    
+    /**
+     * Metodo per la formattazione che converte un oggetto nella re-
+     * lativa rappresentazione di stringa.
+     *
+     * @return stringa dell'oggetto convertito
+     */
     @Override
     public String toString() {
         return "Tree{" +
@@ -32,7 +54,13 @@ public final class Tree<T> {
                 ", listaFigli=" + listaFigli +
                 '}';
     }
-
+    
+    /**
+     * Metodo che controlla se una categoria è foglia.
+     * 
+     * @return TRUE se la categoria è foglia
+     *         FALSE se la categoria non è foglia
+     */
     public boolean isFoglia() {
         return this.listaFigli.size() == 0;
     }
@@ -40,7 +68,13 @@ public final class Tree<T> {
 //    public void setPadre(Tree<T> padre) {
 //        this.padre = padre;
 //    }
-
+    
+    /**
+     * Metodo che aggiunge un figlio (categoria) all'albero.
+     * 
+     * @param figlio la categoria figlio
+     * @return l'albero finale
+     */
     public Tree<T> aggiungiFiglio(T figlio) {
         Tree<T> figlioTree = new Tree<>(figlio);
 
