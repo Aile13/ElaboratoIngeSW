5e4f3ced752157457346ce40eddb9c2891642ba7
Ali <47888612+ljAli@users.noreply.github.com>
2022-03-22 18:55:32 +0100

    Javadoc Managaeable.java

--- src/java/it/unibs/elabingesw/businesslogic/gestione/Manageable.java
+++ src/java/it/unibs/elabingesw/businesslogic/gestione/Manageable.java
@@ -1,8 +1,19 @@
 package it.unibs.elabingesw.businesslogic.gestione;
 
 /**
- * @author Elia
+ * Interfaccia Manageable.
+ * 
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public interface Manageable {
+    
+    /**
+     * Firma di un metodo che controlla se un nome
+     * inserito è uguale o meno a uno già presente
+     * nel sistema.
+     * 
+     * @param nome il nome da controllare
+     */
     boolean isStessoNome(String nome);
 }
