84c7379b3e487fa0b853dd6b24d68e7ee83632e5
Ali <47888612+ljAli@users.noreply.github.com>
2022-03-23 11:29:36 +0100

    Javadoc GestoreGerarchie.java

--- src/java/it/unibs/elabingesw/businesslogic/gestione/GestoreGerarchie.java
+++ src/java/it/unibs/elabingesw/businesslogic/gestione/GestoreGerarchie.java
@@ -5,24 +5,43 @@ import it.unibs.elabingesw.businesslogic.categoria.GerarchiaDiCategorie;
 import java.util.List;
 
 /**
- * @author Elia
+ * Classe GestoreGerarchie, figlia della classe GestoreGenerico.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public final class GestoreGerarchie extends GestoreGenerico<GerarchiaDiCategorie> {
 
     private static final String FILE_NAME = "Gerarchie";
-
+    
+    /**
+     * Costruttore di classe.
+     */
     public GestoreGerarchie() {
         super(FILE_NAME);
     }
-
+    
+    /**
+     * Metodo getter.
+     *
+     * @return la lista delle gerarchie
+     */
     public List<GerarchiaDiCategorie> getListaGerarchie() {
         return this.getListaElementi();
     }
-
+    
+    /**
+     * Metodo che permette di salvare le gerarchie inserite.
+     */
     public void salvaGerarchie() {
         salvaDati();
     }
-
+    
+    /**
+     * Metodo che permette di inserire una gerarchia.
+     *
+     * @param gerarchiaDiCategorie una gerarchia
+     */
     public void inserisciNuovaGerarchia(GerarchiaDiCategorie gerarchiaDiCategorie) {
         this.inserisciElemento(gerarchiaDiCategorie);
     }
