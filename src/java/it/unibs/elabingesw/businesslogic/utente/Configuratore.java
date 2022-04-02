e4fee49ca6d887980923078c27f69cf67ac9795f
Ali <47888612+ljAli@users.noreply.github.com>
2022-04-02 12:14:12 +0200

    Javadoc Configuratore.java

--- src/java/it/unibs/elabingesw/businesslogic/utente/Configuratore.java
+++ src/java/it/unibs/elabingesw/businesslogic/utente/Configuratore.java
@@ -22,7 +22,12 @@ final public class Configuratore extends Utente {
     public Configuratore(String username, String password) {
         super(username, password);
     }
-
+    
+    /**
+     * Metodo getter.
+     * 
+     * @return il tipo di utente (configuratore in questo caso)
+     */
     @Override
     public UserType getUserType() {
         return UserType.CONFIGURATORE;
@@ -30,7 +35,7 @@ final public class Configuratore extends Utente {
 
     /**
      * Metodo che restituisce un configuratore di default con username
-     * "nuovoConfiguratore" e password "default".
+     * "nuovoconfiguratore" e password "default".
      * 
      * @return un configuratore di default
      */
@@ -39,11 +44,12 @@ final public class Configuratore extends Utente {
     }
     
     /**
-     * Metodo ....
+     * Metodo che controlla se lo username inserito sia uguale allo
+     * username del configuratore di default o meno.
      *
      * @param username lo username del configuratore
-     * @return TRUE
-     *         FALSE
+     * @return TRUE se lo username inserito è uguale a quello di default
+     *         FALSE se lo username inserito è diverso da quello di default
      */
     public static boolean isDefaultConfiguratoreByUsername(String username) {
         return getDefaultConfiguratore().isStessoNome(username);
