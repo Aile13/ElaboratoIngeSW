fb407b2b5c48ef910c9b566704ba235c71ea94e8
Ali <47888612+ljAli@users.noreply.github.com>
2022-04-03 18:03:57 +0200

    Javadoc ListaCampiCompilati.java

--- src/java/it/unibs/elabingesw/businesslogic/offerta/ListaCampiCompilati.java
+++ src/java/it/unibs/elabingesw/businesslogic/offerta/ListaCampiCompilati.java
@@ -10,20 +10,45 @@ import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
- * @author Elia
+ * Classe ListaCampiCompilati che permette al fruitore
+ * di reperire e compilare i campi nativi ed ereditati
+ * relativi ad una catgeoria foglia.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class ListaCampiCompilati implements Serializable {
     private final LinkedHashMap<Campo, String> campiCompilati;
 
+    /**
+     * Costruttore di classe, accetta come parametri due oggetti:
+     * la gerarchia e la categoria foglia selezionate dal fruitore.
+     *
+     * @param gerarchiaSelezionata
+     * @param categoriaFogliaSelezionata
+     */ 
     public ListaCampiCompilati(GerarchiaDiCategorie gerarchiaSelezionata, Categoria categoriaFogliaSelezionata) {
         this.campiCompilati = new LinkedHashMap<>();
         estraiCampiDatiGerarchiaECategoriaFoglia(gerarchiaSelezionata, categoriaFogliaSelezionata);
     }
 
+    /**
+     * Metodo getter.
+     *
+     * @return la lista dei campi compilati
+     */
     public LinkedHashMap<Campo, String> getCampiCompilati() {
         return campiCompilati;
     }
 
+    /**
+     * Metodo che permette di estrarre i campi nativi ed ereditati
+     * relativi alla categoria foglia passata per parametro facente
+     * parte della categoria anch'essa passata per parametro.
+     *
+     * @param gerarchia una gerarchia
+     * @param categoriaFoglia una categoria foglia
+     */ 
     private void estraiCampiDatiGerarchiaECategoriaFoglia(GerarchiaDiCategorie gerarchia, Categoria categoriaFoglia) {
         List<Categoria> listaDiCategoriePadri = gerarchia.getListaDiCategoriePadriByCategoria(categoriaFoglia);
         Collections.reverse(listaDiCategoriePadri);
@@ -35,6 +60,12 @@ public class ListaCampiCompilati implements Serializable {
         categoriaFoglia.getCampiNativi().forEach(campo -> this.campiCompilati.put(campo, null));
     }
 
+    /**
+     * Metodo per la formattazione che converte un oggetto nella re-
+     * lativa rappresentazione di stringa.
+     *
+     * @return stringa dell'oggetto convertito
+     */
     @Override
     public String toString() {
         return "ListaCampiCompilati{" +
