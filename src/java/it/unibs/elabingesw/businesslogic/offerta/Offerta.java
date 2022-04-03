1b90b07afd48a1e588a961288810637c3ae05138
Ali <47888612+ljAli@users.noreply.github.com>
2022-04-03 17:49:50 +0200

    Javadoc Offerta.java

--- src/java/it/unibs/elabingesw/businesslogic/offerta/Offerta.java
+++ src/java/it/unibs/elabingesw/businesslogic/offerta/Offerta.java
@@ -8,7 +8,10 @@ import it.unibs.elabingesw.businesslogic.utente.Utente;
 import java.io.Serializable;
 
 /**
- * @author Elia
+ * Classe Offerta che rappresenta un'offerta generica.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class Offerta implements Manageable, Serializable {
     private final String nomeArticolo;
@@ -17,6 +20,16 @@ public class Offerta implements Manageable, Serializable {
     private final ListaCampiCompilati listaCampiCompilati;
     private StatoOfferta statoOfferta;
 
+    /**
+     * Costruttore di classe, accetta come parametri il nome dell'
+     * articolo, l'autore dell'offerta, la lista dei campi compi-
+     * lati e la catgoria di appartenenza dell'articolo.
+     *
+     * @param nomeArticolo
+     * @param autore
+     * @param listaCampiCompilati
+     * @param categoriaDiAppartenenza
+     */ 
     public Offerta(String nomeArticolo, Utente autore, ListaCampiCompilati listaCampiCompilati, Categoria categoriaDiAppartenenza) {
         this.nomeArticolo = nomeArticolo;
         this.autore = autore;
@@ -24,24 +37,57 @@ public class Offerta implements Manageable, Serializable {
         this.listaCampiCompilati = listaCampiCompilati;
         this.statoOfferta = StatoOfferta.APERTA;
     }
-
+    
+    /**
+     * Metodo implementato dall'interfaccia Manageable
+     * che verifica se due articoli hanno lo stesso no-
+     * me o meno.
+     *
+     * @param nomeArticolo il nome dell'articolo
+     * @return TRUE se i nomi sono uguali
+     *         FALSE se i nomi sono diversi
+     */
     @Override
     public boolean isStessoNome(String nomeArticolo) {
         return this.nomeArticolo.equals(nomeArticolo);
     }
-
+    
+    /**
+     * Metodo che controlla se un'offerta è aperta o meno.
+     *
+     * @return TRUE se l'offerta è aperta
+     *         FALSE se l'offerta non è aperta
+     */
     public boolean isOffertaAperta() {
         return this.statoOfferta == StatoOfferta.APERTA;
     }
 
+    /**
+     * Metodo che controlla se due autori sono uguali
+     * o meno.
+     *
+     * @param autore l'oggetto di tipo Utente
+     * @return TRUE se gli autori sono uguali
+     *         FALSE se gli autori sono diversi
+     */
     public boolean isStessoAutore(Utente autore) {
         return this.autore.equals(autore);
     }
-
+    
+    /**
+     * Metodo che ritira un'offerta trasformandone lo 
+     * stato in RITIRATA.
+     */
     public void ritiraOfferta() {
         this.statoOfferta = StatoOfferta.RITIRATA;
     }
 
+    /**
+     * Metodo per la formattazione che converte un oggetto nella re-
+     * lativa rappresentazione di stringa.
+     *
+     * @return stringa dell'oggetto convertito
+     */
     @Override
     public String toString() {
         return "Offerta{" +
@@ -52,11 +98,24 @@ public class Offerta implements Manageable, Serializable {
                 ", statoOfferta=" + statoOfferta +
                 '}';
     }
-
+    
+    /**
+     * Metodo getter.
+     *
+     * @return il nome dell'articolo
+     */
     public String getNomeArticolo() {
         return nomeArticolo;
     }
-
+    
+    /**
+     * Metodo che controlla se un articolo appartiene
+     * a una categoria passata per parametro.
+     *
+     * @param categoria l'oggetto di tipo Categoria
+     * @return TRUE se l'articolo appartiene alla categoria in input
+     *         FALSE se l'articolo non appartiene alla categoria in input
+     */
     public boolean appartieneA(Categoria categoria) {
         return this.categoriaDiAppartenenza.equals(categoria);
     }
