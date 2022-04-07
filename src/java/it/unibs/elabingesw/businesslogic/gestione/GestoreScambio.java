84a8a4d6e1a7031f51b5e2488912cf0f794257fb
Ali <47888612+ljAli@users.noreply.github.com>
2022-04-07 13:37:06 +0200

    Javadoc GestoreScambio.java

--- src/java/it/unibs/elabingesw/businesslogic/gestione/GestoreScambio.java
+++ src/java/it/unibs/elabingesw/businesslogic/gestione/GestoreScambio.java
@@ -5,27 +5,55 @@ import it.unibs.elabingesw.businesslogic.scambio.Scambio;
 import java.util.Optional;
 
 /**
- * @author Elia
+ * Classe GestoreScambio, figlia della classe GestoreGenerico.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class GestoreScambio extends GestoreGenerico<Scambio> {
     private static final String FILE_NAME = "InfoScambio";
 
+    /**
+     * Costruttore di classe.
+     */
     public GestoreScambio() {
         super(FILE_NAME);
     }
 
+    /**
+     * Metodo che controlla se la lista delle informazioni 
+     * relativa a uno scambio è vuota o meno.
+     *
+     * @return TRUE se le info di uno scambio sono da configurare
+     *         FALSE se le info di uno scambio non sono da configurare
+     */
     public boolean isInfoScambioDaConfigurare() {
         return this.getListaElementi().isEmpty();
     }
 
+    /**
+     * Metodo che permette di salvare le informazioni sugli
+     * scambi inseriti.
+     */
     public void salvaInfoScambio() {
         salvaDati();
     }
 
+    /**
+     * Metodo che imposta le informazioni di uno scambio
+     * passato per parametro.
+     *
+     * @param scambio l'oggetto di tipo Scambio
+     */
     public void impostaInfoDiScambio(Scambio scambio) {
         this.inserisciElemento(scambio);
     }
 
+    /**
+     * Metodo getter.
+     *
+     * @return le informazioni di uno scambio
+     */
     public Optional<Scambio> getInfoDiScambio() {
         return this.getListaElementi().stream().findFirst();
     }
