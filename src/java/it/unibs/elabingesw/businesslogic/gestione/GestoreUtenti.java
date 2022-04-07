5fa3dba4ebcf5dc9e68d275848ad8f7a98206898
Ali <47888612+ljAli@users.noreply.github.com>
2022-04-07 14:28:07 +0200

    Javadoc GestoreUtenti.java

--- src/java/it/unibs/elabingesw/businesslogic/gestione/GestoreUtenti.java
+++ src/java/it/unibs/elabingesw/businesslogic/gestione/GestoreUtenti.java
@@ -6,49 +6,106 @@ import it.unibs.elabingesw.businesslogic.utente.UserType;
 import it.unibs.elabingesw.businesslogic.utente.Utente;
 
 /**
- * @author Elia
+ * Classe GestoreUtenti, figlia della classe GestoreGenerico.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public final class GestoreUtenti extends GestoreGenerico<Utente> {
 
     private static final String FILE_NAME = "Utenti";
 
+    /**
+     * Costruttore di classe.
+     */
     public GestoreUtenti() {
         super(FILE_NAME);
         inserisciDefaultConfiguratore();
     }
 
+    /**
+     * Metodo che permette di inserire il configuratore di
+     * default.
+     */
     private void inserisciDefaultConfiguratore() {
         if (!isElementoInListaByNome(
                 Configuratore.getDefaultConfiguratore().getUsername()))
             this.inserisciElemento(Configuratore.getDefaultConfiguratore());
     }
 
+    /**
+     * Metodo che controlla se un configuratore è di default
+     * o meno.
+     *
+     * @param username lo username del configuratore
+     * @return TRUE se il configuratore è di default
+     *         FALSE se il configuratore non è di default
+     */
     public boolean isDefaultConfiguratore(String username) {
         return Configuratore.isDefaultConfiguratoreByUsername(username);
     }
 
+    /**
+     * Metodo che controlla se un utente è valido
+     * o meno.
+     *
+     * @param username lo username dell'utente
+     * @param password la password dell'utente
+     * @return TRUE se l'utente è valido
+     *         FALSE se l'utente non è valido
+     */
     public boolean isUtenteValido(String username, String password) {
         if (this.trovaElementoConNome(username).isPresent()) {
             return this.trovaElementoConNome(username).get().isPasswordCorretta(password);
         } else return false;
     }
 
+    /**
+     * Metodo che controlla se un utente è già registrato
+     * o meno.
+     *
+     * @param username lo username dell'utente
+     * @return TRUE se l'utente è già registrato
+     *         FALSE se l'utente non è già registrato
+     */
     public boolean isUtenteRegistrato(String username) {
         return isElementoInListaByNome(username);
     }
 
+    /**
+     * Metodo che permette di inserire un nuovo configuratore.
+     *
+     * @param username lo username del configuratore
+     * @param password la password del configuratore
+     */
     public void inserisciNuovoConfiguratore(String username, String password) {
         inserisciElemento(new Configuratore(username, password));
     }
 
+    /**
+     * Metodo che permette di salvare gli utenti inseriti.
+     */
     public void salvaUtenti() {
         salvaDati();
     }
 
+    /**
+     * Metodo che permette di inserire un nuovo fruitore.
+     *
+     * @param username lo username del fruitore
+     * @param password la password del fruitore
+     */
     public void inserisciNuovoFruitore(String username, String password) {
         inserisciElemento(new Fruitore(username, password));
     }
 
+    /**
+     * Metodo che restituisce il tipo di utente una volta
+     * passato come parametro il suo username.
+     *
+     * @param username lo username dell'utente
+     * @return il tipo di utente
+     */
     public UserType getUserTypeByNome(String username) {
         if (trovaElementoConNome(username).isPresent()) {
             return trovaElementoConNome(username).get().getUserType();
@@ -56,6 +113,13 @@ public final class GestoreUtenti extends GestoreGenerico<Utente> {
         else return null;
     }
 
+    /**
+     * Metodo che restituisce l'oggetto Utente una volta
+     * passato come parametro il suo username.
+     *
+     * @param username lo username dell'utente
+     * @return l'oggetto Utente
+     */
     public Utente getUserByNome(String username) {
         if (trovaElementoConNome(username).isPresent()) {
             return trovaElementoConNome(username).get();
