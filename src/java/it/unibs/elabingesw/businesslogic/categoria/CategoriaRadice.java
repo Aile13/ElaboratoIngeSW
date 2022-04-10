e6040ba2723bc7f8f5b24ad823235e5ab40b2c7b
Aile <600eli@gmail.com>
2022-04-10 15:46:11 +0200

    javadoc finale per CategoriaFiglio.java, Categoria.java, CategoriaRadice.java, Campo.java.

--- src/java/it/unibs/elabingesw/businesslogic/categoria/CategoriaRadice.java
+++ src/java/it/unibs/elabingesw/businesslogic/categoria/CategoriaRadice.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 /**
  * Classe CategoriaRadice, semplice sottoclasse di Categoria.
+ * <p>
+ * Invariante di classe: lo stesso della super-classe.
  *
  * @author Elia Pitozzi
  * @author Ali Laaraj
@@ -13,12 +15,24 @@ public final class CategoriaRadice extends Categoria {
 
     /**
      * Costruttore di classe, accetta come parametri il nome,
-     * la descrizione e la lista di campi nativi della catego-
-     * ria radice.
+     * la descrizione e la lista di campi nativi della categoria radice.
+     * <p>
+     * Precondizione: assumo parametri costruttore non nulli.
+     * Assumo inoltre che il nome della categoria radice non sia una stringa vuota e
+     * non coincida con nessun altro nome di nessun'altra
+     * categoria radice già inserita nel sistema.
+     * Assumo che la descrizione non sia una stringa vuota.
+     * Assumo poi che la lista dei campi nativi passata al costruttore non contenga già
+     * alcun campo di default tipico delle categorie radici, e che non contenga due campi
+     * con lo stesso nome.
+     * <p>
+     * Post condizione: inserisce nella lista dei campi nativi oltre a quelli passati
+     * al costruttore come parametro anche quelli di default per una categoria radice.
      *
-     * @param nome
-     * @param descrizione
-     * @param campiNativi
+     * @param nome        nome della categoria radice
+     * @param descrizione descrizione della categoria radice
+     * @param campiNativi lista dei campi nativi della categoria radice
+     *                    in aggiunta a quelli di default
      */
     public CategoriaRadice(String nome, String descrizione, List<Campo> campiNativi) {
         super(nome, descrizione, new LinkedList<>());
@@ -27,8 +41,8 @@ public final class CategoriaRadice extends Categoria {
     }
 
     /**
-     * Metodo per la formattazione che converte un oggetto nella re-
-     * lativa rappresentazione di stringa.
+     * Metodo per la formattazione che converte un oggetto nella
+     * relativa rappresentazione di stringa.
      *
      * @return stringa dell'oggetto convertito
      */
