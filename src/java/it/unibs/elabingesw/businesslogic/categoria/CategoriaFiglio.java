38bfaa98cfdfbad71433d326eb09f835a3a865c9
Aile <600eli@gmail.com>
2022-04-10 15:50:10 +0200

    javadoc finale per CategoriaFiglio.java, Categoria.java, CategoriaRadice.java, Campo.java.

--- src/java/it/unibs/elabingesw/businesslogic/categoria/CategoriaFiglio.java
+++ src/java/it/unibs/elabingesw/businesslogic/categoria/CategoriaFiglio.java
@@ -17,15 +17,21 @@ public final class CategoriaFiglio extends Categoria {
      * la descrizione e la lista di campi nativi della categoria figlio.
      * <p>
      * Precondizione: assumo parametri costruttore non nulli.
+     * Assumo nome della categoria figlio non uguale a stringa vuota e non coincidente con nessun altro
+     * nome di qualsiasi categoria già presente nella gerarchia in cui questa categoria è inserita.
+     * Assumo descrizione diversa da stringa vuota.
+     * Assumo campiNativi contenere campi non omonimi rispetto a tutti i campi
+     * presenti nella lista stessa e tra tutti i campi nativi delle categorie padri di
+     * cui questa categoria è direttamente o indirettamente figlia.
      *
      * @param nome        nome della categoria figlio
      * @param descrizione descrizione della categoria figlio
-     * @param campiNativi campi di nativi della categoria figlio
+     * @param campiNativi campi nativi della categoria figlio
      */
     public CategoriaFiglio(String nome, String descrizione, List<Campo> campiNativi) {
         super(nome, descrizione, campiNativi);
     }
-    
+
     /**
      * Metodo per la formattazione che converte un oggetto nella relativa
      * rappresentazione di stringa.
