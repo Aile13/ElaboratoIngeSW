5a0538bc33b9121d4712534e0df1dd2b8b0cf6a1
Aile <600eli@gmail.com>
2022-04-10 18:58:21 +0200

    javadoc finale per TreeNode.java

--- src/java/it/unibs/elabingesw/businesslogic/categoria/TreeNode.java
+++ src/java/it/unibs/elabingesw/businesslogic/categoria/TreeNode.java
@@ -9,6 +9,9 @@ import java.util.stream.Collectors;
 /**
  * Classe Tree che serve per rappresentare una generica struttura
  * ad albero.
+ * <p>
+ * Invariante di classe: assumo gli attributi immutabili,
+ * dopo la creazione dell'oggetto.
  *
  * @author Elia Pitozzi
  * @author Ali Laaraj
@@ -21,6 +24,14 @@ public final class TreeNode<T extends Manageable> implements Serializable {
     /**
      * Costruttore di classe che accetta come parametro un generico
      * dato (nel nostro caso sarà una categoria).
+     * <p>
+     * Precondizione: assumo il parametro del metodo non nullo e adeguatamente inizializzato.
+     * Post condizione: Ho creato l'istanza che rappresenta il
+     * nodo radice dell'intera nuova struttura ad albero,
+     * a cui eventualmente poi si possono associare dei figli, (ovvero altre
+     * istanze dello stesso tipo).
+     *
+     * @param dato dato associata al nodo radice nella struttara ad albero.
      */
     public TreeNode(T dato) {
         this.dato = dato;
@@ -30,6 +41,13 @@ public final class TreeNode<T extends Manageable> implements Serializable {
 
     /**
      * Costruttore privato di classe.
+     * Precondizione: assumo i parametri del metodo non siano nulli e siano adeguatamente inizializzati.
+     * Post condizione: Ho creato l'istanza di un nodo figlio nella struttura ad albero in costruzione,
+     * dove al nodo appena creato è stato associato un suo dato e vi è poi specificato a quale
+     * altro nodo dello stesso tipo lui è discendente.
+     *
+     * @param parent nodo che è padre del nodo che si sta costruendo.
+     * @param dato   dato associato al nodo figlio in costruzione.
      */
     private TreeNode(T dato, TreeNode<T> parent) {
         this.dato = dato;
@@ -49,7 +67,7 @@ public final class TreeNode<T extends Manageable> implements Serializable {
     /**
      * Metodo getter.
      *
-     * @return l'albero
+     * @return nodo padre del nodo corrente.
      */
     private TreeNode<T> getParent() {
         return parent;
@@ -70,7 +88,10 @@ public final class TreeNode<T extends Manageable> implements Serializable {
 
     /**
      * Metodo che restituisce la lista delle informazioni
-     * nel nodo foglia.
+     * nei nodi foglia.
+     * <p>
+     * Post condizione: a partire dal nodo chiamante (this) si è collezionato
+     * in una lista i dati associati ai suoi nodi figli terminali.
      *
      * @return la lista delle informazioni
      */
@@ -100,9 +121,15 @@ public final class TreeNode<T extends Manageable> implements Serializable {
     /**
      * Metodo che permette di cercare un nodo dato il suo
      * nome passato come parametro.
+     * <p>
+     * Precondizione: assumo parametro non nullo e non uguale a stringa vuota.
+     * Post condizione: ricerca (conclusa) del nodo dato il suo nome attraverso
+     * i vari nodi della struttura ad albero costruita dal loro insieme,
+     * a partire dal nodo su cui si chiama il metodo e non per forza dalla root assoluta
+     * dell'albero di cui il nodo chiamante fa parte.
      *
-     * @param nome il nome del nodo
-     * @return il nodo cercato
+     * @param nome il nome del dato del nodo
+     * @return l'optional col nodo se trovato, o l'optional vuoto se nodo non trovato.
      */
     private Optional<TreeNode<T>> trovaTreeNodeByNomeFromThis(String nome) {
         if (this.dato.isStessoNome(nome)) {
@@ -121,9 +148,15 @@ public final class TreeNode<T extends Manageable> implements Serializable {
     /**
      * Metodo che permette di cercare un nodo dato il suo
      * nome partendo dalla radice dell'albero.
+     * <p>
+     * Precondizione: assumo parametro non nullo e non uguale a stringa vuota.
+     * Post condizione: ricerca (conclusa) del nodo dato il suo nome attraverso
+     * i vari nodi della struttura ad albero costruita dal loro insieme,
+     * a partire dal nodo radice di quest'ultima e quindi dalla root assoluta
+     * dell'albero di cui il nodo chiamante fa parte.
      *
      * @param nome il nome del nodo
-     * @return il nodo cercato
+     * @return l'optional col nodo se trovato, o l'optional vuoto se nodo non trovato.
      */
     private Optional<TreeNode<T>> trovaTreeNodeByNomeFromRoot(String nome) {
         // inizia a cercare da root
@@ -134,6 +167,10 @@ public final class TreeNode<T extends Manageable> implements Serializable {
     /**
      * Metodo che controlla se un nodo con nome passato come
      * parametro sia presente o meno nell'albero.
+     * <p>
+     * Precondizione: assumo parametro non nullo e non uguale a stringa vuota.
+     * Post condizione: si esplicita l'esito della ricerca di un nodo
+     * dato il suo nome associato a partire dalla root assoluta dell'intero albero.
      *
      * @param nome il nome del nodo
      * @return TRUE se il nodo cercato è presente nell'albero
@@ -144,8 +181,8 @@ public final class TreeNode<T extends Manageable> implements Serializable {
     }
 
     /**
-     * Metodo per la formattazione che converte un oggetto nella re-
-     * lativa rappresentazione di stringa.
+     * Metodo per la formattazione che converte un oggetto nella
+     * relativa rappresentazione di stringa.
      *
      * @return stringa dell'oggetto convertito
      */
@@ -174,6 +211,8 @@ public final class TreeNode<T extends Manageable> implements Serializable {
     /**
      * Metodo che indenta di un tab una stringa passata come
      * parametro.
+     * <p>
+     * Precondizione: assumo parametro non nullo.
      *
      * @return la stringa indentata
      */
@@ -183,9 +222,16 @@ public final class TreeNode<T extends Manageable> implements Serializable {
 
     /**
      * Metodo che aggiunge un figlio (categoria) all'albero.
+     * <p>
+     * Precondizione: assumo parametro non nullo e correttamente inizializzato.
+     * Ovvero assumo che il nome associato al parametro non presenti omonimie con
+     * altri nodi nell'albero in cui viene inserito.
+     * Post condizione: viene creato un nuovo nodo a partire dal dato passato come
+     * parametro, quindi rispetto al nodo chiamante questo nodo viene inserito
+     * nella sua lista dei figli. Datando l'albero, nel complesso, di un nuovo nodo.
      *
-     * @param figlio la categoria figlio
-     * @return l'albero finale
+     * @param figlio per noi poi la categoria figlio inserita nell'albero.
+     * @return l'albero finale, ovvero il nodo corrente dotato del nuovo nodo tra i suoi figli.
      */
     public TreeNode<T> aggiungiFiglio(T figlio) {
         TreeNode<T> figlioTreeNode = new TreeNode<>(figlio, this);
@@ -196,10 +242,14 @@ public final class TreeNode<T extends Manageable> implements Serializable {
 
     /**
      * Metodo che restituisce la lista delle informazioni
-     * nell'albero padre, il quale nome è passato come pa-
-     * rametro.
+     * degli alberi padre del nodo con nome specificato dal parametro del metodo.
+     * <p>
+     * Precondizione: assumo parametro non nullo e non vuoto.
+     * Post condizione: Si ritorna una lista vuota o con elementi a
+     * seconda che si sia o meno trovato un nodo con medesimo nome rispetto a quello di parametro.
+     * Quindi se il nodo è stato trovato si ricercano in funzione di esso i data dei suoi nodi padre.
      *
-     * @param nome il nome dell'albero padre
+     * @param nome il nome del nodo di cui si considerano gli alberi padre
      * @return la lista delle informazioni
      */
     public List<T> getListOfDataInTreeNodePadriByNome(String nome) {
@@ -213,8 +263,12 @@ public final class TreeNode<T extends Manageable> implements Serializable {
      * Metodo che restituisce la lista delle informazioni
      * nel'albero padre, il quale oggetto corrispondente
      * è passato come parametro.
+     * <p>
+     * Precondizione: assumo parametro non nullo e quindi correttamente inizializzato.
+     * Post condizione: colleziono i data associati a nodi padre del nodo di riferimento
+     * passato come parametro.
      *
-     * @param treeNode l'albero di tipo TreeNode
+     * @param treeNode un istanza della classe.
      * @return la lista delle informazioni
      */
     private List<T> getListOfDataInTreeNodePadriByTreeNode(TreeNode<T> treeNode) {
