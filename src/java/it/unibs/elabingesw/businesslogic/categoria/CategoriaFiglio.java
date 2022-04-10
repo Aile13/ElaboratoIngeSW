e54f266e6f62cd44c56978b1ccda889d7d0d2146
Aile <600eli@gmail.com>
2022-04-10 15:08:49 +0200

    javadoc finale per CategoriaFiglio.java

--- src/java/it/unibs/elabingesw/businesslogic/categoria/CategoriaFiglio.java
+++ src/java/it/unibs/elabingesw/businesslogic/categoria/CategoriaFiglio.java
@@ -4,29 +4,29 @@ import java.util.List;
 
 /**
  * Classe CategoriaFiglio, semplice sottoclasse di Categoria.
- *
+ * <p>
  * Invariante di classe: lo stesso della superclasse.
  *
  * @author Elia Pitozzi
  * @author Ali Laaraj
  */
 public final class CategoriaFiglio extends Categoria {
-    
+
     /**
      * Costruttore di classe, accetta come parametri il nome,
      * la descrizione e la lista di campi nativi della catego-
      * ria figlio.
-     *
+     * <p>
      * Precondizione: assumo parametri costruttore non nulli.
-     * 
-     * @param nome nome delle categoria figlio
+     *
+     * @param nome        nome delle categoria figlio
      * @param descrizione descrizione della categoria figlio
      * @param campiNativi campi di nativi della categoria figlio
      */
     public CategoriaFiglio(String nome, String descrizione, List<Campo> campiNativi) {
         super(nome, descrizione, campiNativi);
     }
-    
+
     /**
      * Metodo per la formattazione che converte un oggetto nella re-
      * lativa rappresentazione di stringa.
