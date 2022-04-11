1d48a12b9711de5741180459ba534c977dc1faaf
Ali <47888612+ljAli@users.noreply.github.com>
2022-04-11 15:46:00 +0200

    Javadoc Manageable.java

--- src/java/it/unibs/elabingesw/businesslogic/gestione/Manageable.java
+++ src/java/it/unibs/elabingesw/businesslogic/gestione/Manageable.java
@@ -1,8 +1,32 @@
 package it.unibs.elabingesw.businesslogic.gestione;
 
 /**
- * @author Elia
+ * Interfaccia Manageable.
+ * <p>
+ * Invariante di classe: l'oggetto di tipo Manageable
+ * deve avere inizializzato un nome o degli attributi
+ * che possono ricostruirne uno per poter identificare
+ * univocamente l'oggetto mediante il metodo di
+ * questa interfaccia.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public interface Manageable {
+    
+    /**
+     * Firma di un metodo che controlla se
+     * il nome passato come parametro
+     * coincide con il nome associato all'oggetto
+     * su cui si richiama il metodo stesso.
+     * <p>
+     * Precondizione: l'oggetto di tipo Manageable
+     * deve avere un nome associato per poter operare
+     * un confronto con il parametro del metodo.
+     * Inoltre si assume che il parametro non sia nullo
+     * o coincidente con la stringa vuota.
+     *
+     * @param nome il nome da usare nel confronto
+     */
     boolean isStessoNome(String nome);
 }
