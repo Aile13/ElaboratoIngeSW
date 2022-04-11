324f284e9d83a666a6631bef1b24d6a83cbebfbf
Aile <600eli@gmail.com>
2022-04-11 16:57:03 +0200

    javadoc finale per Utente.java, Configuratore.java, Fruitore.java, UserType.java.

--- src/java/it/unibs/elabingesw/businesslogic/utente/UserType.java
+++ src/java/it/unibs/elabingesw/businesslogic/utente/UserType.java
@@ -1,8 +1,14 @@
 package it.unibs.elabingesw.businesslogic.utente;
 
 /**
- * Enumeration UserType che definisce i tipi di utente,
+ * Enumeration UserType che definisce i tipi concreti di utente,
  * ossia Fruitore e Configuratore.
+ * <p>
+ * Invariante di classe: una istanza concreta di utente deve
+ * essere per forza una implementazione o da parte di Configuratore
+ * o da parte di Fruitore. Per tanto i tipi che UserType deve tener
+ * conto e valutare sono solo quelli per questi due
+ * tipi di utenti.
  *
  * @author Elia Pitozzi
  * @author Ali Laaraj
