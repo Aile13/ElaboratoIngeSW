2b60caef455ab602caaf1d46bfe4fe3a08f16b59
Aile <600eli@gmail.com>
2022-04-11 16:40:45 +0200

    javadoc finale per Utente.java, Configuratore.java.

--- src/java/it/unibs/elabingesw/businesslogic/utente/Configuratore.java
+++ src/java/it/unibs/elabingesw/businesslogic/utente/Configuratore.java
@@ -1,45 +1,57 @@
 package it.unibs.elabingesw.businesslogic.utente;
 
-import java.io.Serializable;
-
 /**
  * Classe Configuratore, sottoclasse di Utente. Oltre ai metodi ereditati da
- * Utente, contine al suo interno metodi per la gestione del configuratore di
+ * Utente, contiene al suo interno metodi per la gestione del configuratore di
  * default.
+ * <p>
+ * Invariante di classe: lo stesso della super-classe.
  *
  * @author Elia Pitozzi
  * @author Ali Laaraj
  */
 final public class Configuratore extends Utente {
-    
+
     /**
      * Costruttore di classe che accetta come parametri lo username e la
      * password del Configuratore.
+     * <p>
+     * Precondizione: quella del costruttore della super-classe.
+     * Post condizione: quella del costruttore della super-classe.
      *
-     * @param username
-     * @param password
+     * @param username username del configuratore
+     * @param password password del configuratore
      */
     public Configuratore(String username, String password) {
         super(username, password);
     }
-    
+
     /**
-     * Metodo che restituisce un configuratore di default con username
+     * Metodo che restituisce il configuratore di default con username
      * "nuovoconfiguratore" e password "default".
-     * 
-     * @return un configuratore di default
+     * <p>
+     * Post condizione: assumo che lo username e la password
+     * del configuratore di default rimangano immutabili e
+     * coincidenti a quelli espressi nella descrizione del metodo.
+     * Assumo inoltre che esiste, a livello logico, un solo configuratore
+     * di default, identificato da un solo specifico username.
+     *
+     * @return il configuratore di default
      */
     public static Configuratore getDefaultConfiguratore() {
         return new Configuratore("nuovoconfiguratore", "default");
     }
-    
+
     /**
      * Metodo che controlla se il configuratore con lo username
      * passato per parametro è di default o meno.
      *
+     * Precondizione: assumo il parametro non nullo, e
+     * diverso da stringa vuota.
+     *
      * @param username lo username del configuratore
      * @return TRUE se il configuratore è di default
-     *         FALSE se il configuratore non è di default
+     * FALSE se il configuratore non è di default
      */
     public static boolean isDefaultConfiguratoreByUsername(String username) {
         return getDefaultConfiguratore().isStessoNome(username);
