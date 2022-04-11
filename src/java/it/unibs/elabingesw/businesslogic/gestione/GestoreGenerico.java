7ee6991a97b43a56f4b31e305162dfce29600318
Aile <600eli@gmail.com>
2022-04-11 12:16:50 +0200

    javadoc finale per GestoreGenerico.java, GestoreGerarchie.java, GestoreUtenti.java, Manageable.java, GestoreScambio.java.

--- src/java/it/unibs/elabingesw/businesslogic/gestione/GestoreGenerico.java
+++ src/java/it/unibs/elabingesw/businesslogic/gestione/GestoreGenerico.java
@@ -10,6 +10,9 @@ import java.util.Optional;
 /**
  * Classe GestoreGenerico che verrà ereditata dalle classi
  * GestoreUtenti e GestoreGerarchie.
+ * <p>
+ * Invariante di classe: assumo gli attributi immutabili,
+ * dopo la creazione dell'oggetto.
  *
  * @author Elia Pitozzi
  * @author Ali Laaraj
@@ -18,12 +21,19 @@ abstract class GestoreGenerico<T extends Manageable & Serializable> {
     private final String pathRepository;
     private final String dataDir = "./Dati/";
     private final List<T> listaElementi;
-    
+
     /**
      * Costruttore di classe che accetta come parametro il
      * nome di un file.
+     * Precondizione: assumo parametro costruttore non nullo.
+     * Inoltre assumo parametro non uguale a stringa vuota.
+     * Post condizione: costruisco, in funzione del parametro,
+     * il percorso su cui operare la lettura del file
+     * dove eventualmente i dati sono memorizzati.
+     * Quindi a seconda del contenuto del file e delle sua presenza
+     * aggiungo o meno gli elementi ottenuti dalla lettura.
      *
-     * @param fileName
+     * @param fileName nome del file da leggere, senza estensione.
      */
     public GestoreGenerico(String fileName) {
         this.pathRepository = dataDir + fileName + ".dat";
@@ -31,10 +41,13 @@ abstract class GestoreGenerico<T extends Manageable & Serializable> {
         this.listaElementi = new ArrayList<>();
         caricaElementi();
     }
-    
+
     /**
-     * Metodo che crea una directory creando prima tutte le 
-     * directory padre inesistenti.
+     * Metodo che crea una directory se questa non è
+     * già presente, e non lancia eccezione se questa è
+     * già presente.
+     * <p>
+     * Post condizione: directory dati ora sicuramente presente.
      */
     private void inizializzaDirDati() {
         try {
@@ -43,22 +56,24 @@ abstract class GestoreGenerico<T extends Manageable & Serializable> {
             e.printStackTrace();
         }
     }
-    
+
     /**
      * Metodo che salva i dati su file.
      */
     public void salvaDati() {
-        try (ObjectOutputStream output =
-                     new ObjectOutputStream(new FileOutputStream(pathRepository))) {
+        try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(pathRepository))) {
             output.writeObject(this.listaElementi);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+
     /**
-     * Metodo che carica tutti gli utenti e tutte le gerar-
-     * chie su file.
+     * Metodo che carica, dal file, tutti gli elementi in lista.
+     * <p>
+     * Post condizione: se il file esiste allora lo si legge e
+     * si caricano in lista gli elementi letti in esso. Se il file non è
+     * presente allora non si opera alcun caricamento.
      */
     private void caricaElementi() {
         if (new File(pathRepository).exists()) {
@@ -72,19 +87,23 @@ abstract class GestoreGenerico<T extends Manageable & Serializable> {
             }
         }
     }
-    
+
     /**
      * Metodo che controlla se un elemento è presente
-     * nella lista o meno.
+     * nella lista o meno, dell'elemento è fornito solo
+     * il suo nome associato.
+     * <p>
+     * Precondizione: assumo parametro non nullo
+     * e diverso da stringa vuota.
      *
      * @param nome il nome dell'elemento
      * @return TRUE se l'elemento è presente in lista
-     *         FALSE se l'elemento non è presente in lista
+     * FALSE se l'elemento non è presente in lista
      */
     public boolean isElementoInListaByNome(String nome) {
         return trovaElementoConNome(nome).isPresent();
     }
-    
+
     /**
      * Metodo getter.
      *
@@ -93,28 +112,39 @@ abstract class GestoreGenerico<T extends Manageable & Serializable> {
     public List<T> getListaElementi() {
         return listaElementi;
     }
-    
+
     /**
      * Metodo che permette di cercare un elemento
-     * dato il suo nome passato come parametro.
+     * dato il suo nome associato passato come parametro.
+     * <p>
+     * Precondizione: assumo parametro non nullo
+     * e diverso da stringa vuota.
+     * Post condizione: ritorna l'Optional di tipo T,
+     * inizializzato con un elemento se ne è stato
+     * trovato uno con lo stesso nome. Altrimenti
+     * l'Optional ritornato è vuoto.
      *
      * @param nome il nome dell'elemento
-     * @return l'elemento cercato
+     * @return l'optional dell'elemento cercato
      */
     public Optional<T> trovaElementoConNome(String nome) {
-        for (T elemento :
-                listaElementi) {
-            if (elemento.isStessoNome(nome))
-                return Optional.of(elemento);
+        for (T elemento : listaElementi) {
+            if (elemento.isStessoNome(nome)) return Optional.of(elemento);
         }
 
         return Optional.empty();
     }
-    
+
     /**
-     * Metodo che permette di inserire un elemento.
+     * Metodo che permette d'inserire un elemento.
+     * <p>
+     * Precondizione: assumo il parametro non nullo e
+     * correttamente inizializzato. Ovvero che sia
+     * dotato di un nome univoco rispetto a tutti
+     * gli altri elementi della lista in cui viene
+     * aggiunto.
      *
-     * @param e un oggetto generico
+     * @param e un oggetto generico di tipo T.
      */
     public void inserisciElemento(T e) {
         this.listaElementi.add(e);
--- src/java/it/unibs/elabingesw/businesslogic/gestione/GestoreGerarchie.java
+++ src/java/it/unibs/elabingesw/businesslogic/gestione/GestoreGerarchie.java
@@ -5,7 +5,11 @@ import it.unibs.elabingesw.businesslogic.categoria.GerarchiaDiCategorie;
 import java.util.List;
 
 /**
- * Classe GestoreGerarchie, figlia della classe GestoreGenerico.
+ * Classe GestoreGerarchie, sotto-classe della classe GestoreGenerico.
+ * <p>
+ * Invariante di classe: lo stesso della super-classe.
+ * Inoltre il tipo generico T è settato immutabilmente con
+ * il tipo GerarchiaDiCategorie.
  *
  * @author Elia Pitozzi
  * @author Ali Laaraj
@@ -13,34 +17,49 @@ import java.util.List;
 public final class GestoreGerarchie extends GestoreGenerico<GerarchiaDiCategorie> {
 
     private static final String FILE_NAME = "Gerarchie";
-    
+
     /**
      * Costruttore di classe.
+     * <p>
+     * Post condizione: Quella del costruttore della super-classe.
      */
     public GestoreGerarchie() {
         super(FILE_NAME);
     }
-    
+
     /**
      * Metodo getter.
+     * <p>
+     * Post condizione: essendo un metodo che delega,
+     * la post condizione è quella del metodo chiamato.
      *
      * @return la lista delle gerarchie
      */
     public List<GerarchiaDiCategorie> getListaGerarchie() {
         return this.getListaElementi();
     }
-    
+
     /**
-     * Metodo che permette di salvare le gerarchie inserite.
+     * Metodo che permette di salvare in maniera persistente
+     * le gerarchie presenti in lista.
+     * <p>
+     * Post condizione: quella del metodo chiamato.
      */
     public void salvaGerarchie() {
         salvaDati();
     }
-    
+
     /**
-     * Metodo che permette di inserire una gerarchia.
+     * Metodo che permette d'inserire una gerarchia.
+     * <p>
+     * Precondizione: Assumo che il parametro non sia nullo, e
+     * che sia correttamente inizializzato. Ovvero che
+     * vi sia settato almeno l'attributo nome, e che questo
+     * risulti univoco rispetto a tutti gli altri nomi
+     * associati alle gerarchie già presenti in lista.
+     * Post condizione: quella del metodo chiamato.
      *
-     * @param gerarchiaDiCategorie una gerarchia
+     * @param gerarchiaDiCategorie la gerarchia da inserire
      */
     public void inserisciNuovaGerarchia(GerarchiaDiCategorie gerarchiaDiCategorie) {
         this.inserisciElemento(gerarchiaDiCategorie);
