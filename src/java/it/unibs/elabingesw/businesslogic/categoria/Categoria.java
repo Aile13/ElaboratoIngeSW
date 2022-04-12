bddb789e11b0570c3ee65add2bb72b75d741ee0d
Aile <600eli@gmail.com>
2022-04-12 14:43:50 +0200

    javadoc finale per Campo.java, Categoria.java, GerarchiaDiCategorie.java.

--- src/java/it/unibs/elabingesw/businesslogic/categoria/Categoria.java
+++ src/java/it/unibs/elabingesw/businesslogic/categoria/Categoria.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 /**
  * Classe Categoria che rappresenta una categoria generica.
+ * <p>
+ * Invariante di classe: assumo gli attributi immutabili,
+ * dopo la creazione dell'oggetto.
  *
  * @author Elia Pitozzi
  * @author Ali Laaraj
@@ -15,21 +18,23 @@ public class Categoria implements Manageable, Serializable {
     private final String nome;
     private final String descrizione;
     private final List<Campo> campiNativi;
-    
+
     /**
      * Costruttore di classe, accetta come parametri il nome, la
      * descrizione e la lista dei campi nativi di una categoria.
+     * <p>
+     * Precondizione: assumo parametri costruttore non nulli.
      *
-     * @param nome
-     * @param descrizione
-     * @param campiNativi
-     */ 
+     * @param nome        nome della categoria
+     * @param descrizione descrizione della categoria
+     * @param campiNativi campi nativi associati alla categoria
+     */
     public Categoria(String nome, String descrizione, List<Campo> campiNativi) {
         this.nome = nome;
         this.descrizione = descrizione;
         this.campiNativi = campiNativi;
     }
-    
+
     /**
      * Metodo getter.
      *
@@ -47,36 +52,38 @@ public class Categoria implements Manageable, Serializable {
     public String getNome() {
         return nome;
     }
-    
+
     /**
-     * Metodo che permette all'utente di inserire una lista di
+     * Metodo che permette all'utente d'inserire una lista di
      * campi nativi per una determinata categoria.
-     * 
-     * @param campi la lista dei campi
+     * <p>
+     * Precondizione: assumo parametro metodo non nullo.
+     *
+     * @param campi la lista dei campi da aggiungere
      */
     public void inserisciCampiNativi(List<Campo> campi) {
         this.campiNativi.addAll(campi);
     }
-    
+
     /**
-     * Metodo per la formattazione che converte un oggetto nella re-
-     * lativa rappresentazione di stringa.
+     * Metodo per la formattazione che converte un oggetto nella relativa
+     * rappresentazione di stringa.
      *
      * @return stringa dell'oggetto convertito
      */
     @Override
     public String toString() {
-        return  "nome='" + nome + '\'' +
+        return "nome='" + nome + '\'' +
                 ", descrizione='" + descrizione + '\'' +
                 ", campiNativi=" + campiNativi;
     }
-    
+
     /**
      * Metodo che permette di confrontare due oggetti.
      *
      * @param o un oggetto generico
      * @return TRUE se i due oggetti sono uguali
-     *         FALSE se i due oggetti sono diversi
+     * FALSE se i due oggetti sono diversi
      */
     @Override
     public boolean equals(Object o) {
@@ -87,7 +94,7 @@ public class Categoria implements Manageable, Serializable {
 
         return getNome().equals(categoria.getNome());
     }
-    
+
     /**
      * Metodo che fornisce il codice hash dell'oggetto.
      *
@@ -97,21 +104,23 @@ public class Categoria implements Manageable, Serializable {
     public int hashCode() {
         return getNome().hashCode();
     }
-    
+
     /**
      * Metodo implementato dall'interfaccia Manageable
      * che verifica se due gerarchie hanno lo stesso no-
      * me o meno.
+     * <p>
+     * Precondizione: assumo parametro del metodo non nullo.
      *
      * @param nome il nome della gerarchia
      * @return TRUE se i nomi sono uguali
-     *         FALSE se i nomi sono diversi
+     * FALSE se i nomi sono diversi
      */
     @Override
     public boolean isStessoNome(String nome) {
         return this.getNome().equals(nome);
     }
-    
+
     /**
      * Metodo toString ridotto in cui mostro a video
      * solo il nome e la descrizione della categoria.
@@ -124,12 +133,12 @@ public class Categoria implements Manageable, Serializable {
     }
 
     /**
-     * Metodo che controlla se un campo è omonimo 
+     * Metodo che controlla se un campo è omonimo
      * di un altro o meno.
      *
      * @param campo un oggetto Campo
      * @return TRUE se il campo è già stato preso
-     *         FALSE se il campo non è già stato preso
+     * FALSE se il campo non è già stato preso
      */
     public boolean isCampoGiaPreso(Campo campo) {
         return campo.isCampoInListaByNome(campiNativi);
--- src/java/it/unibs/elabingesw/businesslogic/categoria/GerarchiaDiCategorie.java
+++ src/java/it/unibs/elabingesw/businesslogic/categoria/GerarchiaDiCategorie.java
@@ -8,9 +8,12 @@ import java.util.List;
 /**
  * Classe GerarchiaDiCategorie che implementa l'interfaccia Manageable
  * e che gestisce una gerarchia di categorie.
+ * <p>
+ * Invariante di classe: assumo l'attributo immutabile,
+ * dopo la creazione dell'oggetto.
  *
  * @author Elia Pitozzi
- * @auhor Ali Laaraj
+ * @author Ali Laaraj
  */
 public class GerarchiaDiCategorie implements Manageable, Serializable {
     private final TreeNode<Categoria> gerarchia;
@@ -18,8 +21,18 @@ public class GerarchiaDiCategorie implements Manageable, Serializable {
     /**
      * Costruttore privato di classe che accetta come parametro una
      * gerarchia (oggetto Tree<Categoria>).
+     * <p>
+     * Precondizione: assumo che il parametro del costrutto non sia nullo.
+     * Inoltre assumo che sia correttamente configurato; Ovvero che al
+     * livello più alto della gerarchia vi sia una categoria radice non nulla e
+     * inizializzata correttamente, quindi poi a più livelli sottostanti
+     * risiedano eventuali categorie figlie, anche quest'ultime inizializzate correttamente.
+     * Ciascuna delle quali infatti è identificata da un nome univoco tra di esse
+     * e anche con la categoria radice.
+     * Ciascuna categoria figlia deve inoltre garantire campi nativi non omonimi rispetto agli altri campi
+     * nativi delle sue categorie padri.
      *
-     * @param gerarchia
+     * @param gerarchia gerarchia delle categorie
      */
     private GerarchiaDiCategorie(TreeNode<Categoria> gerarchia) {
         this.gerarchia = gerarchia;
@@ -37,8 +50,12 @@ public class GerarchiaDiCategorie implements Manageable, Serializable {
     /**
      * Costruttore di classe che accetta come parametro una categoria
      * radice.
+     * <p>
+     * Precondizione: assumo il parametro non nullo e
+     * correttamente inizializzato. Ovvero non omonimia della categoria radice
+     * passata come parametro con altre categorie radice di altre gerarchie già esistenti.
      *
-     * @param categoriaRadice
+     * @param categoriaRadice root della gerarchia
      * @see TreeNode
      */
     public GerarchiaDiCategorie(Categoria categoriaRadice) {
@@ -76,10 +93,12 @@ public class GerarchiaDiCategorie implements Manageable, Serializable {
      * Metodo implementato dall'interfaccia Manageable
      * che verifica se due gerarchie hanno lo stesso no-
      * me o meno.
+     * <p>
+     * Precondizione: assumo parametro del metodo non nullo.
      *
      * @param nome il nome della gerarchia
      * @return TRUE se i nomi sono uguali
-     *         FALSE se i nomi sono diversi
+     * FALSE se i nomi sono diversi
      */
     @Override
     public boolean isStessoNome(String nome) {
@@ -111,6 +130,14 @@ public class GerarchiaDiCategorie implements Manageable, Serializable {
     /**
      * Metodo che aggiunge una categoria figlio passata come parametro
      * a una gerarchia di categorie.
+     * <p>
+     * Precondizione: Assumo che la categoria figlia passata
+     * come parametro non sia nulla e sia correttamente inizializzata.
+     * Ovvero che soddisfi la condizione di non omonimia rispetto alle categorie già presenti
+     * nella medesima gerarchia in cui la categoria figlia sta per essere inserita.
+     * Inoltre assumo che i campi nativi di essa non siano omonimi rispetto
+     * ai campi nativi delle sue categorie padri in tal gerarchia.
+     * Infine assumo la non omonimia tra gli stessi campi nativi della categoria figlia.
      *
      * @param categoriaFiglio una categoria figlio
      * @return una gerarchia di categorie con all'interno la categoria figlio
@@ -119,18 +146,20 @@ public class GerarchiaDiCategorie implements Manageable, Serializable {
         TreeNode<Categoria> categoriaFiglioTreeNode = this.gerarchia.aggiungiFiglio(categoriaFiglio);
         return new GerarchiaDiCategorie(categoriaFiglioTreeNode);
     }
-    
+
     /**
-     * Metodo che controlla se il nome della categoria passato come
-     * parametro è già stato usato o meno.
+     * Metodo che controlla se il nome di una categoria passato
+     * come parametro sia già stato utilizzato o meno.
+     * <p>
+     * Precondizione: assumo parametro non nullo.
      *
-     * @return TRUE se il nome è già stato usato
-     *         FALSE se il nome della categoria non è già stato usato
+     * @return TRUE se il nome della categoria è già utilizzato
+     * FALSE se il nome della categoria non è già utilizzato
      */
     public boolean isNomeCategoriaUsato(String nomeCategoria) {
         return gerarchia.isPresentTreeNodeByNome(nomeCategoria);
     }
-    
+
     /**
      * Metodo toString ridotto in cui mostro a video
      * solo il nome e la descrizione delle categorie
@@ -143,10 +172,14 @@ public class GerarchiaDiCategorie implements Manageable, Serializable {
                 '\t' + this.gerarchia.getDato().toStringRidotto() + '\n'
                 + "}";
     }
-    
+
     /**
      * Metodo che ritorna la lista delle categorie padre
      * passata come parametro una determinata categoria.
+     * <p>
+     * Precondizione: assumo categoria parametro non nullo.
+     * Inoltre assumo che la categoria passata come parametro
+     * sia inizializzata almeno con il suo attributo nome.
      *
      * @param categoria l'oggetto Categoria
      * @return la lista delle categorie padre
@@ -173,4 +206,5 @@ public class GerarchiaDiCategorie implements Manageable, Serializable {
         }
         return this.gerarchia.getDato().isCampoGiaPreso(campo);
     }
+
 }
