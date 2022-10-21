6487ee89e0ab2e69242cc885dc1219e824e8e39e
Ali <47888612+ljAli@users.noreply.github.com>
2022-10-21 17:27:24 +0200

    Javadoc Login.java V2

--- src/it/unibs/elabingesw/mainservice/Login.java
+++ src/it/unibs/elabingesw/mainservice/Login.java
@@ -19,7 +19,7 @@ public class Login {
      * Costruttore di classe, accetta come parametro un oggetto
      * GestoreUtenti.
      *
-     * @param gestoreUtenti
+     * @param gestoreUtenti oggetto di tipo GestoreUtenti
      * @see GestoreUtenti
      */
     public Login(GestoreUtenti gestoreUtenti) {
