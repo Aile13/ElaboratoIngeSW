63f5fa5f9c9d80435d9dcc8bd018c6a34278b57a
Ali <47888612+ljAli@users.noreply.github.com>
2023-01-22 12:07:05 +0100

    Javadoc ScambioRepository.java

--- src/it/unibs/elabingesw/businesslogic/repository/ScambioRepository.java
+++ src/it/unibs/elabingesw/businesslogic/repository/ScambioRepository.java
@@ -5,14 +5,47 @@ import it.unibs.elabingesw.businesslogic.scambio.Scambio;
 import java.util.Optional;
 
 /**
- * @author Elia
+ * Interfaccia ScambioRepository con metodi appositi
+ * per gestire operazioni sugli scambi.
+ * 
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public interface ScambioRepository {
+    
+    /**
+     * Metodo che controlla se la lista delle informazioni
+     * relativa a uno scambio è vuota o meno.
+     *
+     * @return TRUE se le info di uno scambio sono da configurare
+     * FALSE se le info di uno scambio non sono da configurare
+     */
     boolean isInfoScambioDaConfigurare();
 
+    /**
+     * Metodo che permette di salvare le informazioni sugli
+     * scambi inserite.
+     */
     void salvaInfoScambio();
 
+    /**
+     * Metodo che imposta le informazioni di uno scambio
+     * passato per parametro.
+     * <p>
+     * Precondizioni: assumo parametro non nullo e
+     * correttamente inizializzato.
+     * Inoltre assumo che il metodo in questione venga invocato solo
+     * quando le info di scambio non sono già state impostate.
+     * Post condizione: Quella del metodo chiamato.
+     *
+     * @param scambio l'oggetto di tipo Scambio
+     */
     void impostaInfoDiScambio(Scambio scambio);
 
+    /**
+     * Metodo getter.
+     *
+     * @return le informazioni di scambio
+     */
     Optional<Scambio> getInfoDiScambio();
 }
