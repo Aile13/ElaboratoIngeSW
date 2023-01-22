a9310260ec6612774b94cfe543d0d937939ec322
Ali <47888612+ljAli@users.noreply.github.com>
2023-01-22 17:27:48 +0100

    Javadoc IntervalloOrariServiceView.java

--- src/it/unibs/elabingesw/view/IntervalloOrariServiceView.java
+++ src/it/unibs/elabingesw/view/IntervalloOrariServiceView.java
@@ -3,18 +3,38 @@ package it.unibs.elabingesw.view;
 import it.unibs.eliapitozzi.mylib.InputDati;
 
 /**
- * @author Elia
+ * Classe IntervalloOrariServiceView, sottoclasse di View.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class IntervalloOrariServiceView extends View {
 
+    /**
+     * Metodo che chiede all'utente se vuole aggiungere un 
+     * intervallo orario o meno.
+     *
+     * @return TRUE se si vuole aggiungere un altro intervallo orario
+     * FALSE se non si vuole aggiungere un altro intervallo orario
+     */
     public boolean chiediSeAggiungereAltroIntervalloOrario() {
         return InputDati.yesOrNo("Vuoi aggiungere un altro intervallo orario?");
     }
 
+    /**
+     * Metodo che chiede all'utente di inserire l'ora
+     *
+     * @return l'ora
+     */
     public int chiediOreDiOrario() {
         return InputDati.leggiIntero("Inserisci solo l'ora (24h): ", 0, 23);
     }
 
+    /**
+     * Metodo che chiede all'utente di inserire i minuti
+     *
+     * @return i minuti
+     */
     public int chiediMinutiDiOrario() {
         return InputDati.leggiIntero("Inserisci solo i minuti (0 o 30): ");
     }
