e2271c1058ff4eee516ce06fe99230c47a02846d
Ali <47888612+ljAli@users.noreply.github.com>
2023-01-22 17:24:57 +0100

    Javadoc ListaCampiCompilatiServiceView.java

--- src/it/unibs/elabingesw/view/ListaCampiCompilatiServiceView.java
+++ src/it/unibs/elabingesw/view/ListaCampiCompilatiServiceView.java
@@ -3,17 +3,43 @@ package it.unibs.elabingesw.view;
 import it.unibs.eliapitozzi.mylib.InputDati;
 
 /**
- * @author Elia
+ * Classe ListaCampiCompilatiServiceView, sottoclasse di View.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class ListaCampiCompilatiServiceView extends View {
+    
+    /**
+     * Metodo che chiede all'utente di compilare il campo 
+     * obbligatorio che viene passato per parametro.
+     *
+     * @param nomeCampo il nome del campo
+     * @return la compilazione del campo
+     */
     public String chiediCompilazioneCampoObbligatorioByNome(String nomeCampo) {
         return InputDati.leggiStringaNonVuota("Compila campo obbligatorio " + nomeCampo + ": ");
     }
 
+    /**
+     * Metodo che chiede all'utente se compilare il campo 
+     * non obbligatorio che viene passato per parametro.
+     *
+     * @param nomeCampo il nome del campo
+     * @return TRUE se si vuole compilare il campo non obbligatorio
+     * FALSE se non si vuole compilare il campo non obbligatorio
+     */
     public boolean chiediSeCompilareCampoNonObbligatorioByNome(String nomeCampo) {
         return InputDati.yesOrNo("Compilare campo non obbligatorio " + nomeCampo + " ? ");
     }
 
+    /**
+     * Metodo che chiede all'utente di compilare il campo non 
+     * non obbligatorio il cui nome è passato per parametro.
+     *
+     * @param nomeCampo il nome del campo
+     * @return la compilazione del campo
+     */
     public String chiediCompilazioneCampoNonObbligatorioByNome(String nomeCampo) {
         return InputDati.leggiStringaNonVuota("Compila campo " + nomeCampo + ": ");
     }
