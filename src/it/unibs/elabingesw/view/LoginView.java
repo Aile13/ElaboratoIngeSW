0e4c5ef02d11a96f3c2159db7c4ebd24091ea871
Ali <47888612+ljAli@users.noreply.github.com>
2023-01-22 17:35:41 +0100

    Javadoc LoginView.java

--- src/it/unibs/elabingesw/view/LoginView.java
+++ src/it/unibs/elabingesw/view/LoginView.java
@@ -3,26 +3,61 @@ package it.unibs.elabingesw.view;
 import it.unibs.eliapitozzi.mylib.InputDati;
 
 /**
- * @author Elia
+ * Classe LoginView, sottoclasse di View.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class LoginView extends View {
 
+    /**
+     * Metodo che imposta la password per lo username
+     * passato per parametro
+     *
+     * @param username lo username
+     * @return il messaggio di impostazione password
+     */
     public String setPasswordByUsername(String username) {
         return InputDati.leggiStringaNonVuota("Imposta password per " + username + ": ");
     }
 
+    /**
+     * Metodo che chiede all'utente di inserire la 
+     * password.
+     *
+     * @return il messaggio di inserimento password
+     */
     public String getPasswordString() {
         return InputDati.leggiStringaNonVuota("Inserisci password: ");
     }
 
+    /**
+     * Metodo che chiede all'utente di inserire lo 
+     * username.
+     *
+     * @return il messaggio di inserimento username
+     */
     public String getUsernameString() {
         return InputDati.leggiStringaNonVuota("Inserisci username: ");
     }
 
+    /**
+     * Metodo che chiede all'utente se confermare la sua re-
+     * gistrazione come nuovo fruitore.
+     *
+     * @return TRUE se ci si registra da nuovi fruitori
+     * FALSE se non ci si registra da nuovi fruitori
+     */
     public boolean chiediConfermaRegistrazioneFruitore() {
         return InputDati.yesOrNo("Si desidera registrarsi come nuovo fruitore? ");
     }
 
+    /**
+     * Metodo che chiede all'utente di inserire il 
+     * nuovo username.
+     *
+     * @return il messaggio di inserimento username
+     */
     public String getNewUsernameString() {
         return InputDati.leggiStringaNonVuota("Inserisci nuovo username: ");
     }
