82372ab80baddccc8dcc9f47460207ee950a1212
Ali <47888612+ljAli@users.noreply.github.com>
2023-01-22 11:35:01 +0100

    Javadoc GerarchiaRepository.java

--- src/it/unibs/elabingesw/businesslogic/repository/GerarchiaRepository.java
+++ src/it/unibs/elabingesw/businesslogic/repository/GerarchiaRepository.java
@@ -5,17 +5,50 @@ import it.unibs.elabingesw.businesslogic.categoria.GerarchiaDiCategorie;
 import java.util.List;
 
 /**
- * @author Elia
+ * Interfaccia GerarchiaRepository con metodi appositi
+ * per gestire operazioni su collezioni di gerarchie.
+ * 
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public interface GerarchiaRepository {
+    
+    /**
+     * Metodo getter.
+     *
+     * @return la lista delle gerarchie
+     */
     List<GerarchiaDiCategorie> getListaGerarchie();
 
+    /**
+     * Metodo che permette di salvare in maniera persistente
+     * le gerarchie presenti in lista.
+     */
     void salvaGerarchie();
 
+    /**
+     * Metodo che inserisce una nuova gerarchia nella lista di gerarchie.
+     *
+     * @param gerarchiaDiCategorie oggetto di tipo GerarchiaDiCategorie
+     */
     void inserisciNuovaGerarchia(GerarchiaDiCategorie gerarchiaDiCategorie);
 
-    // TODO: Laaraj 27/nov/2022 Aggiungere doc. metodo nuovo.
+    /**
+     * Metodo che controlla se una gerarchia il cui nome è
+     * passato per parametro è presente in lista o meno.
+     *
+     * @param nomeGerarchia il nome della gerarchia
+     * @return TRUE la gerarchie è presente in lista
+     * FALSE la gerarchia non è presente in lista
+     */
     boolean isGerarchiaPresenteByNome(String nomeGerarchia);
 
+    /**
+     * Metodo che controlla se la lista delle gerarchie
+     * contiene almeno una gerarchia.
+     *
+     * @return TRUE se la lista delle gerarchie non è vuota
+     * FALSE se la lista delle gerarchie è vuota
+     */
     boolean haGerarchie();
 }
