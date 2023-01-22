e2d2e6fb60b648d67b730724b32590e27fdbf5da
Ali <47888612+ljAli@users.noreply.github.com>
2023-01-22 17:29:50 +0100

    Javadoc View.java

--- src/it/unibs/elabingesw/view/View.java
+++ src/it/unibs/elabingesw/view/View.java
@@ -3,9 +3,20 @@ package it.unibs.elabingesw.view;
 import java.util.Observable;
 
 /**
- * @author Elia
+ * Classe View che estende Observable per poter
+ * applicare il pattern MVC.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class View extends Observable {
+    
+    /**
+     * Metodo che visualizza il messaggio passato per 
+     * parametro.
+     *
+     * @param messaggio il messaggio da mostrare a video
+     */
     public void visualizzaMessaggio(String messaggio) {
         System.out.println(messaggio);
     }
