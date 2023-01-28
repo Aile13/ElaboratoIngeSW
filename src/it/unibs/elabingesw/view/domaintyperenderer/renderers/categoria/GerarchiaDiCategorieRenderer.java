08d9d1f67ecf2350537d44e855a57e5b82945bb0
Ali <47888612+ljAli@users.noreply.github.com>
2023-01-28 13:39:11 +0100

    Javadoc GerarchiaDiCategorieRenderer.java

--- src/it/unibs/elabingesw/view/domaintyperenderer/renderers/categoria/GerarchiaDiCategorieRenderer.java
+++ src/it/unibs/elabingesw/view/domaintyperenderer/renderers/categoria/GerarchiaDiCategorieRenderer.java
@@ -6,15 +6,35 @@ import it.unibs.elabingesw.view.domaintyperenderer.CompositeDomainTypeRenderer;
 import it.unibs.elabingesw.view.domaintyperenderer.SelectableDomainTypeRenderer;
 
 /**
- * @author Elia
+ * Classe GerarchiaDiCategorieRenderer che implementa SelectableDomainTypeRenderer.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class GerarchiaDiCategorieRenderer implements SelectableDomainTypeRenderer {
+    
+    /**
+     * Metodo che restituisce il rendering dell'oggetto in input
+     * a cui verrà applicato un cast.
+     *
+     * @param domainTypeToRender l'oggetto di tipo DomainTypeToRender
+     * @return il rendering della gerarchia
+     */
     @Override
     public String render(DomainTypeToRender domainTypeToRender) {
         GerarchiaDiCategorie gerarchiaDiCategorie = (GerarchiaDiCategorie) domainTypeToRender;
         return "Gerarchia " + gerarchiaDiCategorie.getNome() + " {\n" + new CompositeDomainTypeRenderer().render(gerarchiaDiCategorie.getGerarchia()) + "}";
     }
 
+    /**
+     * Metodo che controlla se il renderer può gestire
+     * il rendering dell'oggetto che viene passato per
+     * parametro.
+     *
+     * @param domainTypeToRender l'oggetto di tipo DomainTypeToRender
+     * @return TRUE se si può gestire il rendering della gerarchia
+     * FALSE se non si può gestire il rendering della gerarchia
+     */
     @Override
     public boolean canHandle(DomainTypeToRender domainTypeToRender) {
         return domainTypeToRender instanceof GerarchiaDiCategorie;
