e24dc4d22e8fc2d4ccc008dd2d560bf4fa81cd2c
Ali <47888612+ljAli@users.noreply.github.com>
2023-01-28 13:33:20 +0100

    Javadoc CategoriaRadiceRenderer.java

--- src/it/unibs/elabingesw/view/domaintyperenderer/renderers/categoria/CategoriaRadiceRenderer.java
+++ src/it/unibs/elabingesw/view/domaintyperenderer/renderers/categoria/CategoriaRadiceRenderer.java
@@ -5,14 +5,34 @@ import it.unibs.elabingesw.businesslogic.categoria.CategoriaRadice;
 import it.unibs.elabingesw.view.domaintyperenderer.SelectableDomainTypeRenderer;
 
 /**
- * @author Elia
+ * Classe CategoriaRadiceRenderer che implementa SelectableDomainTypeRenderer.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class CategoriaRadiceRenderer implements SelectableDomainTypeRenderer {
+    
+    /**
+     * Metodo che restituisce il rendering dell'oggetto in input
+     * a cui verrà applicato un cast.
+     *
+     * @param domainTypeToRender l'oggetto di tipo DomainTypeToRender
+     * @return il rendering della categoria radice
+     */
     @Override
     public String render(DomainTypeToRender domainTypeToRender) {
         return "CategoriaRadice{ " + new CategoriaRenderer().render(domainTypeToRender) + " }";
     }
 
+    /**
+     * Metodo che controlla se il renderer può gestire
+     * il rendering dell'oggetto che viene passato per
+     * parametro.
+     *
+     * @param domainTypeToRender l'oggetto di tipo DomainTypeToRender
+     * @return TRUE se si può gestire il rendering della categoria radice
+     * FALSE se non si può gestire il rendering della categoria radice
+     */
     @Override
     public boolean canHandle(DomainTypeToRender domainTypeToRender) {
         return domainTypeToRender instanceof CategoriaRadice;
