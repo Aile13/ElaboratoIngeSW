f302cedf04df521e57c338dfaaf07a4bf22aae6d
Ali <47888612+ljAli@users.noreply.github.com>
2023-01-28 13:37:14 +0100

    Javadoc CategoriaRenderer.java

--- src/it/unibs/elabingesw/view/domaintyperenderer/renderers/categoria/CategoriaRenderer.java
+++ src/it/unibs/elabingesw/view/domaintyperenderer/renderers/categoria/CategoriaRenderer.java
@@ -9,9 +9,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * @author Elia
+ * Classe CategoriaRenderer che implementa SelectableDomainTypeRenderer.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class CategoriaRenderer implements SelectableDomainTypeRenderer {
+    
+    /**
+     * Metodo che restituisce il rendering dell'oggetto in input
+     * a cui verrà applicato un cast.
+     *
+     * @param domainTypeToRender l'oggetto di tipo DomainTypeToRender
+     * @return il rendering della categoria
+     */
     @Override
     public String render(DomainTypeToRender domainTypeToRender) {
         Categoria categoria = (Categoria) domainTypeToRender;
@@ -20,11 +31,26 @@ public class CategoriaRenderer implements SelectableDomainTypeRenderer {
                 ", campiNativi=" + renderListaDiCampiNativi(categoria.getCampiNativi());
     }
 
+    /**
+     * Metodo che restituisce il rendering della lista dei campi
+     * nativi di una categoria passati come parametro.
+     *
+     * @param campiNativi la lista dei campi nativi della categoria
+     * @return il rendering della lista dei campi nativi
+     */
     private String renderListaDiCampiNativi(List<Campo> campiNativi){
         return "[" + campiNativi.stream().map(campo -> new CampoRenderer().render(campo)).collect(Collectors.joining(", ")) +
                 "]";
     }
 
+    /**
+     * Metodo che controlla se il renderer può gestire
+     * il rendering dell'oggetto che viene passato per
+     * parametro.
+     *
+     * @param domainTypeToRender l'oggetto di tipo DomainTypeToRender
+     * @return FALSE non si può gestire il rendering della categoria
+     */
     @Override
     public boolean canHandle(DomainTypeToRender domainTypeToRender) {
         return false;
