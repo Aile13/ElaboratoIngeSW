a3e5661c11f1de6cf1c67dc3c00ec9aa1359e1df
Ali <47888612+ljAli@users.noreply.github.com>
2023-01-29 12:02:57 +0100

    Javadoc CategoriaRadiceLimitedRenderer.java

--- src/it/unibs/elabingesw/view/domaintypelimitedrenderer/limitedrenderers/categoria/CategoriaRadiceLimitedRenderer.java
+++ src/it/unibs/elabingesw/view/domaintypelimitedrenderer/limitedrenderers/categoria/CategoriaRadiceLimitedRenderer.java
@@ -5,14 +5,34 @@ import it.unibs.elabingesw.businesslogic.categoria.CategoriaRadice;
 import it.unibs.elabingesw.view.domaintypelimitedrenderer.SelectableDomainTypeLimitedRenderer;
 
 /**
- * @author Elia
+ * Classe CategoriaRadiceLimitedRenderer che implementa SelectableDomainTypeLimitedRenderer.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class CategoriaRadiceLimitedRenderer implements SelectableDomainTypeLimitedRenderer {
+    
+    /**
+     * Metodo che restituisce il rendering limitato dell'oggetto
+     * in input.
+     *
+     * @param domainTypeToLimitedRender l'oggetto di tipo DomainTypeToLimitedRender
+     * @return il rendering limitato della categoria radice
+     */
     @Override
     public String render(DomainTypeToLimitedRender domainTypeToLimitedRender) {
         return "CategoriaRadice{ " + new CategoriaLimitedRenderer().render(domainTypeToLimitedRender)+ " }";
     }
 
+    /**
+     * Metodo che controlla se il renderer può gestire
+     * il rendering limitato dell'oggetto che viene 
+     * passato per parametro.
+     *
+     * @param domainTypeToLimitedRender l'oggetto di tipo DomainTypeToLimitedRender
+     * @return TRUE se si può gestire il rendering limitato della categoria radice
+     * FALSE se non si può gestire il rendering limitato della categoria radice
+     */
     @Override
     public boolean canHandle(DomainTypeToLimitedRender domainTypeToLimitedRender) {
         return domainTypeToLimitedRender instanceof CategoriaRadice;
