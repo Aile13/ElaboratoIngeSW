e198f8432a4f63e77267bf1c4c68ecebbbd0a92e
Ali <47888612+ljAli@users.noreply.github.com>
2023-01-29 11:00:19 +0100

    Javadoc UtenteRenderer.java

--- src/it/unibs/elabingesw/view/domaintyperenderer/renderers/utente/UtenteRenderer.java
+++ src/it/unibs/elabingesw/view/domaintyperenderer/renderers/utente/UtenteRenderer.java
@@ -5,15 +5,35 @@ import it.unibs.elabingesw.businesslogic.utente.Utente;
 import it.unibs.elabingesw.view.domaintyperenderer.SelectableDomainTypeRenderer;
 
 /**
- * @author Elia
+ * Classe UtenteRenderer che implementa SelectableDomainTypeRenderer.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class UtenteRenderer implements SelectableDomainTypeRenderer {
+    
+    /**
+     * Metodo che restituisce il rendering dell'oggetto in input
+     * a cui verrà applicato un cast.
+     *
+     * @param domainTypeToRender l'oggetto di tipo DomainTypeToRender
+     * @return il rendering dell'utente
+     */
     @Override
     public String render(DomainTypeToRender domainTypeToRender) {
         Utente utente = (Utente) domainTypeToRender;
         return "Utente{" + "username='" + utente.getUsername() + '\'' + '}';
     }
 
+    /**
+     * Metodo che controlla se il renderer può gestire
+     * il rendering dell'oggetto che viene passato per
+     * parametro.
+     *
+     * @param domainTypeToRender l'oggetto di tipo DomainTypeToRender
+     * @return TRUE se si può gestire il rendering dell'utente
+     * FALSE se non si può gestire il rendering dell'utente
+     */
     @Override
     public boolean canHandle(DomainTypeToRender domainTypeToRender) {
         return domainTypeToRender instanceof Utente;
