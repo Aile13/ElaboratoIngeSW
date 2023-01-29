0e1676de470b6e9c675254f4c6027e26a3c64ad7
Ali <47888612+ljAli@users.noreply.github.com>
2023-01-29 10:36:18 +0100

    Javadoc InScambioStateRenderer.java

--- src/it/unibs/elabingesw/view/domaintyperenderer/renderers/offerta/state/InScambioStateRenderer.java
+++ src/it/unibs/elabingesw/view/domaintyperenderer/renderers/offerta/state/InScambioStateRenderer.java
@@ -5,14 +5,34 @@ import it.unibs.elabingesw.businesslogic.offerta.state.InScambioState;
 import it.unibs.elabingesw.view.domaintyperenderer.SelectableDomainTypeRenderer;
 
 /**
- * @author Elia
+ * Classe InScambioStateRenderer che implementa SelectableDomainTypeRenderer.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class InScambioStateRenderer implements SelectableDomainTypeRenderer {
+    
+    /**
+     * Metodo che restituisce il rendering dell'oggetto in input
+     * a cui verrà applicato un cast.
+     *
+     * @param domainTypeToRender l'oggetto di tipo DomainTypeToRender
+     * @return il rendering dello stato dell'offerta
+     */
     @Override
     public String render(DomainTypeToRender domainTypeToRender) {
         return "InScambioState{}";
     }
 
+    /**
+     * Metodo che controlla se il renderer può gestire
+     * il rendering dell'oggetto che viene passato per
+     * parametro.
+     *
+     * @param domainTypeToRender l'oggetto di tipo DomainTypeToRender
+     * @return TRUE se si può gestire il rendering dello stato dell'offerta
+     * FALSE se non si può gestire il rendering dello stato dell'offerta
+     */
     @Override
     public boolean canHandle(DomainTypeToRender domainTypeToRender) {
         return domainTypeToRender instanceof InScambioState;
