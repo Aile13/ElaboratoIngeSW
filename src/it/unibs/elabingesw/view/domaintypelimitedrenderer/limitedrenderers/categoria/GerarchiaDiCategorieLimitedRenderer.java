d5eae0f8864ba031bab16a4b2f2f9d5b21a34c7d
Ali <47888612+ljAli@users.noreply.github.com>
2023-01-29 12:04:40 +0100

    Javadoc GerarchiaDiCategorieLimitedRenderer.java

--- src/it/unibs/elabingesw/view/domaintypelimitedrenderer/limitedrenderers/categoria/GerarchiaDiCategorieLimitedRenderer.java
+++ src/it/unibs/elabingesw/view/domaintypelimitedrenderer/limitedrenderers/categoria/GerarchiaDiCategorieLimitedRenderer.java
@@ -6,15 +6,35 @@ import it.unibs.elabingesw.view.domaintypelimitedrenderer.CompositeDomainTypeLim
 import it.unibs.elabingesw.view.domaintypelimitedrenderer.SelectableDomainTypeLimitedRenderer;
 
 /**
- * @author Elia
+ * Classe GerarchiaDiCategorieLimitedRenderer che implementa SelectableDomainTypeLimitedRenderer.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class GerarchiaDiCategorieLimitedRenderer implements SelectableDomainTypeLimitedRenderer {
+    
+    /**
+     * Metodo che restituisce il rendering limitato dell'oggetto
+     * in input a cui verrà applicato un cast.
+     *
+     * @param domainTypeToLimitedRender l'oggetto di tipo DomainTypeToLimitedRender
+     * @return il rendering limitato della gerarchia
+     */
     @Override
     public String render(DomainTypeToLimitedRender domainTypeToLimitedRender) {
         GerarchiaDiCategorie gerarchiaDiCategorie = (GerarchiaDiCategorie) domainTypeToLimitedRender;
         return "Gerarchia " + gerarchiaDiCategorie.getNome() + " {\n" + '\t' + new CompositeDomainTypeLimitedRenderer().render(gerarchiaDiCategorie.getGerarchia().getDato()) + '\n' + "}";
     }
 
+    /**
+     * Metodo che controlla se il renderer può gestire
+     * il rendering limitato dell'oggetto che viene 
+     * passato per parametro.
+     *
+     * @param domainTypeToLimitedRender l'oggetto di tipo DomainTypeToLimitedRender
+     * @return TRUE se si può gestire il rendering limitato della gerarchia
+     * FALSE se non si può gestire il rendering limitato della gerarchia
+     */
     @Override
     public boolean canHandle(DomainTypeToLimitedRender domainTypeToLimitedRender) {
         return domainTypeToLimitedRender instanceof GerarchiaDiCategorie;
