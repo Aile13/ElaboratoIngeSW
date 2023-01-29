3c0c84bbf3921d10ff41282bbd7716ea7872448b
Ali <47888612+ljAli@users.noreply.github.com>
2023-01-29 10:58:00 +0100

    Javadoc ScambioRenderer.java

--- src/it/unibs/elabingesw/view/domaintyperenderer/renderers/scambio/ScambioRenderer.java
+++ src/it/unibs/elabingesw/view/domaintyperenderer/renderers/scambio/ScambioRenderer.java
@@ -9,9 +9,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * @author Elia
+ * Classe ScambioRenderer che implementa SelectableDomainTypeRenderer.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class ScambioRenderer implements SelectableDomainTypeRenderer {
+    
+    /**
+     * Metodo che restituisce il rendering dell'oggetto in input
+     * a cui verrà applicato un cast.
+     *
+     * @param domainTypeToRender l'oggetto di tipo DomainTypeToRender
+     * @return il rendering dello scambio
+     */
     @Override
     public String render(DomainTypeToRender domainTypeToRender) {
         Scambio scambio = (Scambio) domainTypeToRender;
@@ -24,11 +35,27 @@ public class ScambioRenderer implements SelectableDomainTypeRenderer {
                 "\n" + '}';
     }
 
+    /**
+     * Metodo che restituisce il rendering della lista degli inter-
+     * valli orari passati come parametro.
+     *
+     * @param intervalliOrari la lista degli intervalli orari
+     * @return il rendering della lista degli intervalli orari
+     */
     private String renderListaDiIntervalliOrari(List<IntervalloOrario> intervalliOrari) {
         return "[" + intervalliOrari.stream().map(intervalloOrario -> new IntervalloOrarioRenderer().render(intervalloOrario)).collect(Collectors.joining(", ")) +
                 "]";
     }
 
+    /**
+     * Metodo che controlla se il renderer può gestire
+     * il rendering dell'oggetto che viene passato per
+     * parametro.
+     *
+     * @param domainTypeToRender l'oggetto di tipo DomainTypeToRender
+     * @return TRUE se si può gestire il rendering dello scambio
+     * FALSE se non si può gestire il rendering dello scambio
+     */
     @Override
     public boolean canHandle(DomainTypeToRender domainTypeToRender) {
         return domainTypeToRender instanceof Scambio;
