922f4a16b7d4ee56188df0fd0433882784681294
Ali <laarajali7@gmail.com>
2023-01-29 15:56:48 +0100

    Javadoc OffertaContextRenderer.java

--- src/it/unibs/elabingesw/view/domaintyperenderer/renderers/offerta/OffertaContextRenderer.java
+++ src/it/unibs/elabingesw/view/domaintyperenderer/renderers/offerta/OffertaContextRenderer.java
@@ -7,9 +7,20 @@ import it.unibs.elabingesw.view.domaintyperenderer.CompositeDomainTypeRenderer;
 import it.unibs.elabingesw.view.domaintyperenderer.SelectableDomainTypeRenderer;
 
 /**
- * @author Elia
+ * Classe OffertaContextRenderer che implementa SelectableDomainTypeRenderer.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public class OffertaContextRenderer implements SelectableDomainTypeRenderer {
+
+    /**
+     * Metodo che restituisce il rendering dell'oggetto in input
+     * a cui verrà applicato un cast.
+     *
+     * @param domainTypeToRender l'oggetto di tipo DomainTypeToRender
+     * @return il rendering dell'offerta
+     */
     @Override
     public String render(DomainTypeToRender domainTypeToRender)   {
         OffertaContext offertaContext = (OffertaContext) domainTypeToRender;
@@ -20,6 +31,15 @@ public class OffertaContextRenderer implements SelectableDomainTypeRenderer {
                 ", statoOfferta=" + new CompositeDomainTypeRenderer().render(offertaContext.getOffertaState()) + '}';
     }
 
+    /**
+     * Metodo che controlla se il renderer può gestire
+     * il rendering dell'oggetto che viene passato per
+     * parametro.
+     *
+     * @param domainTypeToRender l'oggetto di tipo DomainTypeToRender
+     * @return TRUE se si può gestire il rendering dell'offerta
+     * FALSE se non si può gestire il rendering dell'offerta
+     */
     @Override
     public boolean canHandle(DomainTypeToRender domainTypeToRender) {
         return domainTypeToRender instanceof OffertaContext;
