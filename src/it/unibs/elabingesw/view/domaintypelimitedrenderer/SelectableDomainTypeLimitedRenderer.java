9cbec84da71411550e6ef293414f55172dcd33c1
Ali <47888612+ljAli@users.noreply.github.com>
2023-01-29 11:18:10 +0100

    Javadoc SelectableDomainTypeLimitedRenderer.java

--- src/it/unibs/elabingesw/view/domaintypelimitedrenderer/SelectableDomainTypeLimitedRenderer.java
+++ src/it/unibs/elabingesw/view/domaintypelimitedrenderer/SelectableDomainTypeLimitedRenderer.java
@@ -3,10 +3,31 @@ package it.unibs.elabingesw.view.domaintypelimitedrenderer;
 import it.unibs.elabingesw.businesslogic.DomainTypeToLimitedRender;
 
 /**
- * @author Elia
+ * Interfaccia SelectableDomainTypeLimitedRenderer che verrà 
+ * implementata dai vari renderer delle classi di dominio.
+ *
+ * @author Elia Pitozzi
+ * @author Ali Laaraj
  */
 public interface SelectableDomainTypeLimitedRenderer {
+    
+    /**
+     * Metodo che restituisce il rendering dell'oggetto che
+     * viene passato per parametro.
+     *
+     * @param domainTypeToLimitedRender l'oggetto di tipo DomainTypeToLimitedRender
+     * @return il rendering limitato
+     */
     String render(DomainTypeToLimitedRender domainTypeToLimitedRender);
 
+    /**
+     * Metodo che controlla se il renderer può gestire
+     * il rendering dell'oggetto che viene passato per
+     * parametro.
+     *
+     * @param domainTypeToLimitedRender l'oggetto di tipo DomainTypeToLimitedRender
+     * @return TRUE se si può gestire il rendering limitato
+     * FALSE se non si può gestire il rendering limitato
+     */
     boolean canHandle(DomainTypeToLimitedRender domainTypeToLimitedRender);
 }
